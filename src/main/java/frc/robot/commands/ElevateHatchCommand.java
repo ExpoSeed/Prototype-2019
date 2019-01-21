c746cad7757f8d90463d974ca0d9fbbdeab0bc00
ExpoSeed <43502820+SaarthakMohan@users.noreply.github.com>
2019-01-21 14:36:08 -0600

    added Commands to move to heights

---
+++ src/main/java/frc/robot/commands/ElevateHatchCommand.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.ElevatorSys;
+
+public class ElevateHatchCommand extends Command {
+  private ElevatorSys m_elevator;
+  private int num;
+
+  public ElevateHatchCommand(ElevatorSys es, int num) {
+    this.m_elevator = es;
+    this.num = num;
+    requires(m_elevator);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    m_elevator.moveToHatch(num);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return m_elevator.isComplete();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
